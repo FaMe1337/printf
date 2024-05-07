8f7df4bd35f7933d3780becf856ee4491483eb9d
FaMe1337 <167479661+FaMe1337@users.noreply.github.com>
2024-05-07 01:51:45 +0100

    rdy for evaluation

--- libftprintf.h
+++ libftprintf.h
@@ -18,13 +18,11 @@
 # include <stdio.h>
 
 int	ft_printf(const char *s, ...);
-int	ft_printf_char(const char c, int i);
+int	ft_printf_char(int c, int i);
 int	ft_printf_str(const char *str, int i);
-int	ft_printf_ptr(void *str, int i);
-int	ft_printf_dec(int nb, int i);
+int	ft_printf_ptr(unsigned long long ptr, int i);
 int	ft_printf_num(int nb, int i);
 int	ft_printf_unsdec(unsigned int nb, int i);
-int	ft_printf_hex_low(unsigned int nb, int i);
-int	ft_printf_hex_up(unsigned int, int i);
+int	ft_printf_hex(unsigned int nb, int i, char specifier);
 
 #endif
