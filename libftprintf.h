30c649e02285c342c31dbbc25955eda9b01f432d
FaMe1337 <167479661+FaMe1337@users.noreply.github.com>
2024-05-09 21:58:20 +0100

    UPS

--- libftprintf.h
+++ libftprintf.h
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <unistd.h>
